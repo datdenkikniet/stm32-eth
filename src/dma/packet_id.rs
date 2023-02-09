93c8d49b8fb53d4446eab69570ca4b1fb4d839e2
datdenkikniet <jcdra1@gmail.com>
2023-02-09 13:30:59 +0100

    Make PacketId NonZeroU32 to avoid layout waste

--- src/dma/packet_id.rs
+++ src/dma/packet_id.rs
@@ -1,3 +1,5 @@
+use nonmax::NonMaxU32;
+
 /// A packet ID.
 ///
 /// This packet ID can be used to obtain information about a specific
@@ -9,9 +11,15 @@
 The main use is obtaining timestamps for frames using [`EthernetDMA::get_timestamp_for_id`](crate::EthernetDMA::get_timestamp_for_id)
 "
 )]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(Debug, PartialEq, Clone, Copy)]
-pub struct PacketId(pub u32);
+pub struct PacketId(pub NonMaxU32);
+
+#[cfg(feature = "defmt")]
+impl defmt::Format for PacketId {
+    fn format(&self, fmt: defmt::Formatter) {
+        defmt::write!(fmt, "PacketId({})", self.0.get())
+    }
+}
 
 impl PacketId {
     /// The initial value for an [`Option<PacketId>`]
@@ -20,6 +28,13 @@ impl PacketId {
 
 impl From<u32> for PacketId {
     fn from(value: u32) -> Self {
+        let value = if value == u32::MAX {
+            // SAFETY: 0 != u32::MAX
+            unsafe { NonMaxU32::new_unchecked(0) }
+        } else {
+            // SAFETY: value != u32::MAX
+            unsafe { NonMaxU32::new_unchecked(value) }
+        };
         Self(value)
     }
 }
